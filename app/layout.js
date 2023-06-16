1cb9a6c2e8da1b1920d916c1769b45f0d9be9528
dracu-lah <nevilnicks4321@gmail.com>
2023-06-16 19:01:57 +0530

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,21 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Nevil | DVLPR",
   description: "Portfolio website of a frontend developer",
-  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
       <body className={inter.className}>
-
         {children}
+        <Analytics />
       </body>
     </html>
   );
