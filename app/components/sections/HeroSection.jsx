7cd0cd7143be13f0f8b5ea7fc92061307ff212c0
dracu-lah <110589548+dracu-lah@users.noreply.github.com>
2023-06-23 09:23:32 -0700

    Development (#15)
    * fixed animations in hero section
    
    * added typewriter effect
    
    * hero section centered
    
    * fixed weird white tint issue in white mode

--- app/components/sections/HeroSection.jsx
+++ app/components/sections/HeroSection.jsx
@@ -6,27 +6,29 @@ const HeroSection = () => {
   return (
     <section
       id="hero"
-      className="min-h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden "
+      className="min-h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden pt-40"
     >
-      <HeroCircles />
-      <HeroImage />
-      <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
-          Learner
-        </h2>
-        <TypeWriterText />
-        <div className=" pt-5 flex justify-center items-center gap-x-2 ">
-          <Link href="#about" className="heroButton ">
-            About
-          </Link>
-          <Link href="#skills" className="heroButton ">
-            Skills
-          </Link>
-          <Link href="#portfolio" className="heroButton">
-            Projects
-          </Link>
+
+        <HeroCircles />
+        <HeroImage />
+        <div className="z-20">
+          <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+            Learner
+          </h2>
+          <TypeWriterText />
+          <div className=" pt-5 flex justify-center items-center gap-x-2 ">
+            <Link href="#about" className="heroButton ">
+              About
+            </Link>
+            <Link href="#skills" className="heroButton ">
+              Skills
+            </Link>
+            <Link href="#portfolio" className="heroButton">
+              Projects
+            </Link>
+          </div>
+
         </div>
-      </div>
     </section>
   );
 };
