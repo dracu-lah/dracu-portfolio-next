8ce721dd663dfcd5549000bb1e57768533832e72
dracu-lah <110589548+dracu-lah@users.noreply.github.com>
2023-06-23 06:01:11 -0700

    Development (#14)
    * fixed animations in hero section
    
    * added typewriter effect

--- app/components/sections/HeroSection.jsx
+++ app/components/sections/HeroSection.jsx
@@ -1,71 +1,20 @@
-"use client";
 import Link from "next/link";
-import React, { useEffect } from "react";
-import { useAnimate } from "framer-motion";
-import Image from "next/image";
 import HeroImage from "../HeroImage";
-async function getData() {
-  const response = await databases.listDocuments(database, hero_image_url_cid);
-  return response;
-}
-
+import HeroCircles from "../HeroCircles";
+import TypeWriterText from "../TypeWriterText";
 const HeroSection = () => {
-  const [scope, animate] = useAnimate();
-  useEffect(() => {
-    animate(
-      scope.current,
-      { opacity: [0, 1], scale: [0.5, 1] },
-      {
-        duration: 0.8,
-        delay: 0.5,
-        ease: [0, 0.71, 0.2, 1.01],
-      }
-    );
-    animate(
-      "article",
-      { opacity: [0, 1], scale: [0.5, 1] },
-      {
-        duration: 0.3,
-        ease: [0, 0.71, 0.2, 1.01],
-        scale: {
-          type: "spring",
-          damping: 5,
-          stiffness: 100,
-          restDelta: 0.001,
-        },
-      }
-    );
-  });
   return (
     <section
-      ref={scope}
       id="hero"
       className="min-h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden "
     >
-      <article
-        className="relative flex justify-center items-center"
-        style={{
-          opacity: 1,
-          borderRadius: "79.9639%",
-          transform: "none",
-        }}
-      >
-        <div className="absolute rounded-full border border-[#333333] h-[200px] w-[200px] mt-52 animate-ping"></div>
-        <div className="absolute rounded-full border border-[#333333] h-[300px] w-[300px] mt-52 "></div>
-        <div className="absolute rounded-full border border-[#333333] h-[500px] w-[500px] mt-52 "></div>
-        <div className="absolute rounded-full border border-yellow-300 h-[650px] w-[650px] mt-52 opacity-20 animate-pulse"></div>
-        <div className="absolute rounded-full border border-[#333333] h-[800px] w-[800px] mt-52 "></div>
-      </article>
+      <HeroCircles />
       <HeroImage />
       <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           Learner
         </h2>
-        <h1 className="text-4xl md:text-6xl font-semibold px-10">
-          {"<Frontend Developer "}
-          <span className="animate-pulse text-yellow-400">/</span>
-          {">"}
-        </h1>
+        <TypeWriterText />
         <div className=" pt-5 flex justify-center items-center gap-x-2 ">
           <Link href="#about" className="heroButton ">
             About
