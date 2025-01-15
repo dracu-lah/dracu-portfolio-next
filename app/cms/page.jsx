cc64d7eac1113f0ecf98d0abae95cb037740b6fa
dracu-lah <nevilnicks4321@gmail.com>
2025-01-15 03:06:20 -0800

    updated jsconfig

--- app/cms/page.jsx
+++ app/cms/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-import React from "react";
 import { useAuth } from "@/hooks/useAuth";
 import LoginForm from "@/components/cms/LoginForm";
 import ProjectsList from "@/components/cms/ProjectsList";
