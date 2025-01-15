d2f3e2d37bb577429f85ecbf8964cd86cad1d75e
dracu-lah <nevilnicks4321@gmail.com>
2025-01-15 02:53:23 -0800

    enabled images from all placed

--- next.config.js
+++ next.config.js
@@ -25,22 +25,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-        pathname:
-          "/images/9fhnx7v3/production/c946b693b9f9bee92ac6a700bcecd84cc8c6c35f-2754x2754.jpg",
-      },
-      {
-        protocol: "https",
-        hostname: "v1.tailwindcss.com",
-        port: "",
-        pathname: "/img/card-top.jpg",
-      },
-      {
-        protocol: "https",
-        hostname: "cloud.appwrite.io",
-        port: "",
-        pathname: "/v1/storage/buckets/**",
+        hostname: "**", // Allow images from any hostname
       },
     ],
   },
