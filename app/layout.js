f8c3d99726dfebc0fe53a0a8118e8269ceedeaa3
dracu-lah <nevilnicks4321@gmail.com>
2025-05-24 13:29:07 +0530

    removed font

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,5 @@
-import Navbar from "./components/Navbar/Navbar";
 import "./globals.css";
-import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Nevil | DVLPR",
@@ -13,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         {children}
         <Analytics />
       </body>
